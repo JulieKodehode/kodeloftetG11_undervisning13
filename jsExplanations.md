9c86ba16132d09473d944f6bbdf6336fb020388a
JulieKodehode <julie.kodehode@gmail.com>
2024-11-12 12:04:13 +0100

    After lesson: Intro deeper into JS

---
+++ jsExplanations.md
@@ -0,0 +1,49 @@
+# VARIABLER
+const = noe som er konstant, ikke skal endres
+let = noe som kan endres
+
+# DATATYPER
+number = tall
+string = tekst
+boolean = sant eller usant
+array = liste 
+object = lagrer forskjellig data, hver bit har et navn med en verdi, f.eks farge: "rød"
+
+array og object er noe vi kommer tilbake til, trengs ikke å læres nå ;)
+
+undefined = ingenting er her
+null = jeg har bestemt at denne skal være tom
+
+undefined og null er som regel tegn på en error i koden, noe er feil
+
+# FUNKSJON
+function yourFunctionName(bokser for verdier, det kalles arguments){
+  skriv koden her
+  return eller console.log()
+}
+yourFunctionName(verdier, det kalles parametres)
+
+# KOBLE TIL HTML
+document.querySelection('#navnPåId')
+
+# EVENTLISTENER
+variabelNavn.addEventlistener("click", function{
+    hva skal skje når man trykker
+})
+
+# IF ELSE STATEMENTS
+if(){
+
+} = sjekker om noe er sant eller usant
+else{
+
+} = hvis ikke så skjer dette
+
+# OPERATORER
+=== betyr at noe er helt likt
+!= skrives for å sjekke: "hvis ikke dette er likt"
+
+# CLASSLIST
+classList.add = legger til en class
+classlist.remove = tar bort en class
+classlist.toggle = skifter mellom å legge til og ta bort klassenavn
--- script.js
+++ script.js
@@ -1,119 +1,71 @@
-// EKSEMPEL PÅ KODE VI GÅR IGJENNOM
 /*
-IKKE COPY/PASTE, SKRIV SELV ;)
-
-//VARIABLER
-
-const = noe som er konstant, ikke skal endres
-let = noe som kan endres
-
-//DATATYPER
-
-number = tall
-string = tekst
-boolean = sant eller usant
-
-array = liste 
-object = lagrer forskjellig data, hver bit har et navn med en verdi, f.eks farge: "rød"
-
-array og object er noe vi kommer tilbake til, trengs ikke å læres nå ;)
-
-undefined = ingenting er her
-null = jeg har bestemt at denne skal være tom
-
-undefined og null er som regel tegn på en error i koden, noe er feil
-
-
-//FUNKSJON//
-
-function yourFunctionName(bokser for verdier, det kalles arguments){
-    
-    skriv koden her
-
-    return eller console.log()
-}
-
-yourFunctionName(verdier, det kalles parametres)
-
-
-//KOBLE TIL HTML
-
-document.querySelection('#navnPåId')
-
-
-//EVENTLISTENER
-
-variabelNavn.addEventlistener("click", function{
-    hva skal skje når man trykker
-})
-
-//IF ELSE STATEMENTS
-
-if(){
-
-} = sjekker om noe er sant eller usant
-
-else{} = hvis ikke så skjer dette
-
-=== betyr at noe er helt likt
-
-!= skrives for å sjekke: "hvis ikke dette er likt"
-
-
-//CLASSLIST
-
-classList.add = legger til en class
-classlist.remove = tar bort en class
-classlist.toggle = skifter mellom å legge til og ta bort klassenavn
-
 //OPPGAVER
-
  Oppgave1: 
- 1.lag 3 variabler for en fruktbutikk,
- en variabel for butikkens navn, en variabel for antall epler i butikken, 
- og en variabel for om butikken er åpen eller ikke
-
- 2. console.log variablene
-
- 3. Hvilket variabel skal ha const?
-
- BONUS!
-
- 4. lag en array eller et object som viser forskjellig typer frukt i butikken
-
+  1.lag 3 variabler for en fruktbutikk,
+  en variabel for butikkens navn, en variabel for antall epler i butikken, 
+  og en variabel for om butikken er åpen eller ikke
+  2. console.log variablene
+  3. Hvilket variabel skal ha const?
+  BONUS!
+  4. lag en array eller et object som viser forskjellig typer frukt i butikken
 
  Oppgave2: 
-
- 1.skriv en funksjon som console.log "Min første funksjon"
- 2. skriv en funksjon som trekker fra 2 tall, f.eks 7-3,
+  1.skriv en funksjon som console.log "Min første funksjon"
+  2. skriv en funksjon som trekker fra 2 tall, f.eks 7-3,
   consol.log sum 
-
-BONUS!
-
- 3. skriv en funksjon hvor du bruker parametre og argumenter(se over om usikker),
-    funksjonen skal sette sammen et fornavn og et etternavn, 
-    skriv 2 variabler utenfor funksjonen som lagrer funksjonen med ulike verdier, 
-    console.log variabel navnet
-
+  BONUS!
+  3. skriv en funksjon hvor du bruker parametre og argumenter(se over om usikker),
+  funksjonen skal sette sammen et fornavn og et etternavn, 
+  skriv 2 variabler utenfor funksjonen som lagrer funksjonen med ulike verdier, 
+  console.log variabel navnet
 
  Oppgave3:
- 1.Lag en ny knapp i HTML og gi den en id
- 2. Lag en variabel hvor du kobler til den nye knappen
- 3. Lag en eventListener som console.log "knapp er trykket" når du trykker på den
-
- BONUS!
-
- 4. Lag en animasjon i CSS som du skrur av og på med addEventlistener
- 
-
+  1.Lag en ny knapp i HTML og gi den en id
+  2. Lag en variabel hvor du kobler til den nye knappen
+  3. Lag en eventListener som console.log "knapp er trykket" når du trykker på den
+  BONUS!
+  4. Lag en animasjon i CSS som du skrur av og på med addEventlistener
 */
 
-//Datatyper
+// SVAR PÅ OPPGAVE 1:
+const storeName = "Fruity";
+let numberofApples = 10;
+let storeOpen = true;
+console.log(storeName, numberofApples, storeOpen);
 
+let fruitStorage = ["Apples", "Pears", "Cherrys"]
+console.log(fruitStorage);
+
+// SVAR PÅ OPPGAVE 2:
+function myFunction() {
+  console.log("Min første funksjon");
+};
+
+myFunction()
+
+function calculator() {
+  let numberOne = 10;
+  let numberTwo = 5;
+  let sum = numberOne + numberTwo
+  console.log(sum);
+};
+
+calculator()
+
+function personalia(first, last) {
+  let full = first + last;
+  console.log(full);
+};
+
+personalia("Julie", "Hauge");
+
+//Datatyper
 //Number, String, Boloeon
+const name = "Julie";
+let age = 27;
+let isPresent = true;
 
 //Arrays, objects
-
 let passengers = ['Ania Kubow', 'Kevin Powell', 'Simo Edwin', 'Thomas Cargill'];
 
 let flightInfo = {
@@ -131,8 +83,57 @@ let flightInfo = {
   },
 };
 
-//Hvordan skrive en funksjon
+// Hvordan skrive en funksjon
 
-// Argumenter vs parametre
+function addNumbers() {
+  let numOne = 2;
+  let numTwo = 10;
+
+  let sum = numOne + numTwo;
+  console.log(sum);
+};
+
+addNumbers();
+
+// Parameter vs argumenter
+
+function minusNumbers(a, b) {
+  let sum = a - b;
+  console.log(sum);
+
+  return sum;
+}
+minusNumbers(10, 5);
+
+let firstSum = minusNumbers(10, 3);
+let secondSum = minusNumbers(8, 4);
+
+console.log(firstSum);
+console.log(secondSum);
 
 // Hvordan hente inn ting vi skal gjøre noe med
+const toggleCircle = document.querySelector("#toggleCircle");
+console.log(toggleCircle)
+const toggle = document.querySelector("#toggleCont");
+console.log(toggle);
+const button = document.querySelector("#showBtn");
+console.log(button);
+const text = document.querySelector("#text");
+console.log(text);
+
+toggle.addEventListener("click", function () {
+  console.log("Click is registered");
+  if (toggleCircle.classList.contains("move")) {
+    toggleCircle.classList.remove("move");
+    toggleCircle.classList.add("moveBack");
+  } else {
+    toggleCircle.classList.remove("moveback");
+    toggleCircle.classList.add("move");
+  };
+});
+
+button.addEventListener("click", function () {
+  console.log("Click is registered");
+  text.classList.toggle("showTextBox");
+  text.classList.toggle("hiddenBox");
+});
